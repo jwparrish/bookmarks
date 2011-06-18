f4b0d8be4ec47fdcd54834349fde3bd7bbc8d2ed
Jon Parrish <jonparrish@rocketmail.com>
2011-06-18 16:16:35 -0500

    first commit

---
+++ bookmarks/views.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponse
+
+def main_page(request):
+	output = '''
+		<html>
+			<head><title>%s</title></head>
+			<body>
+				<h1>%s</h1><p>%s</p>
+			</body>
+		</html>
+	''' % (
+		'Django Bookmarks',
+		'Welcome to Django Bookmarks',
+		'Where you can store and share bookmarks!'
+	)
+	return HttpResponse(output)
---
+++ urls.py
@@ -0,0 +1,12 @@
+#from django.conf.urls.defaults import patterns, include, url
+from django.conf.urls.defaults import *
+from bookmarks.views import *
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+
+	(r'^$', main_page),
+)
