b85df7dd9882f2100913a7cb527824368b1bb42c
Jon Parrish <jonparrish@rocketmail.com>
2011-06-20 20:38:03 -0500

    Created Login Page

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -6,11 +6,7 @@ from django.contrib.auth.models import User
 
 def main_page(request):
 	template = get_template('main_page.html')
-	variables = Context({
-		'head_title': 'Django Bookmarks',
-		'page_title': 'Welcome to Django Bookmarks',
-		'page_body': 'Where you can store and share bookmarks!'
-	})
+	variables = Context({ 'user': request.user })
 	output = template.render(variables)
 	return HttpResponse(output)
 	
