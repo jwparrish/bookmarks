9918c9bf4fb214c7228db943518420ba7e9c5d93
Jon Parrish <jonparrish@rocketmail.com>
2011-06-23 13:22:52 -0500

    added some account management

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 from django.shortcuts import render_to_response
 from django.contrib.auth import logout
 from django.template import RequestContext
-
+from bookmarks.forms import *
 
 def main_page(request):
 	return render_to_response(
@@ -28,3 +28,20 @@ def user_page(request, username):
 def logout_page(request):
 	logout(request)
 	return HttpResponseRedirect('/')
+	
+def register_page(request):
+	if request.method == 'POST':
+		form = RegistrationForm(request.POST)
+		if form.is_valid():
+			user = User.objects.create_user(
+				username=form.cleaned_data['username'],
+				password=form.cleaned_data['password1'],
+				email=form.cleaned_data['email']
+			)
+			return HttpResponseRedirect('/register/success/')
+	else:
+		form = RegistrationForm()
+	variables = RequestContext(request, {
+		'form': form
+	})
+	return render_to_response('registration/register.html', variables)
--- urls.py
+++ urls.py
@@ -2,6 +2,7 @@
 import os.path
 from django.conf.urls.defaults import *
 from bookmarks.views import *
+from django.views.generic.simple import direct_to_template
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -18,4 +19,7 @@ urlpatterns = patterns('',
 	(r'^logout/$', logout_page),
 	(r'^site_media/(?P<path>.*)$', 'django.views.static.serve',
 		{ 'document_root': site_media }),
+	(r'^register/$', register_page),
+	(r'^register/success/$', direct_to_template,
+		{ 'template': 'registration/register_success.html' })
 )
