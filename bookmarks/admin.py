1a909239f7d5e9ff3d4f4cd0c29db56274ce5508
Jon Parrish <jonparrish@rocketmail.com>
2011-07-13 23:02:52 -0500

    updated Admin listing

--- bookmarks/admin.py
+++ bookmarks/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from bookmarks.models import *
 
+class BookmarkAdmin(admin.ModelAdmin):
+	list_display = ('title', 'link', 'user')
+	list_filter = ('user', )
+	ordering = ('title', )
+	search_fields = ('title', )
+
 admin.site.register(Link)
-admin.site.register(Bookmark)
+admin.site.register(Bookmark, BookmarkAdmin)
 admin.site.register(Tag)
 admin.site.register(SharedBookmark)
