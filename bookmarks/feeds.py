1459fe4e1ab05c53a4fdf1b4aa17269931c910bb
Jon Parrish <jonparrish@rocketmail.com>
2011-07-15 14:16:46 -0500

    pg 173

--- bookmarks/feeds.py
+++ bookmarks/feeds.py
@@ -1,5 +1,7 @@
 from django.contrib.syndication.feeds import Feed
 from bookmarks.models import Bookmark
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.models import User
 
 class RecentBookmarks(Feed):
 	title = 'Django Bookmarks | Recent Bookmarks'
@@ -8,3 +10,18 @@ class RecentBookmarks(Feed):
 	
 	def items(self):
 		return Bookmark.objects.order_by('-id')[:10]
+		
+class UserBookmarks(Feed):
+	def get_object(self, bits):
+		if len(bits) != 1:
+			raise ObjectDoesNotExist
+		return User.objects.get(username=bits[0])
+	def title(self, user):
+		return 'Django Bookmarks | Bookmarks for %s' % user.username
+	def link(self, user):
+		return '/feeds/user/%s/' % user.username
+	def description(self, user):
+		return 'Recent bookmarks posted by %s' % user.username
+	def items(self, user):
+		return user.bookmark_set.order_by('-id')[:10]
+		
