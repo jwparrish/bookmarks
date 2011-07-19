75c4f0906ed8b1faa061a53e84dea7433b659e2a
Jon Parrish <jonparrish@rocketmail.com>
2011-07-18 21:58:03 -0500

    pg 195, Tags Broke at some point

--- urls.py
+++ urls.py
@@ -25,6 +25,7 @@ feeds = {
 urlpatterns = patterns('',
 	#Friends
 	(r'^friends/(\w+)/$', friends_page),
+	(r'^friend/add/$', friend_add),
 		
 	#Feeds
 	(r'^feeds/(?P<url>.*)/$', 'django.contrib.syndication.views.feed', 
