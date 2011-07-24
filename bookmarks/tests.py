dab6bbaad489cf1bbad0c546a1ffe2d3caf19a86
Jon Parrish <jonparrish@rocketmail.com>
2011-07-24 13:56:34 -0500

    error pages ect...

--- bookmarks/tests.py
+++ bookmarks/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.test.client import Client
 
 class ViewTest(TestCase):
+	fixtures = ['test_data.json']
 	def setUp(self):
 		self.client = Client()
 	
@@ -15,3 +16,19 @@ class ViewTest(TestCase):
 		response = self.client.post('/register/', data)
 		self.assertEqual(response.status_code, 302)
 		
+	def test_bookmark_save(self):
+		response = self.client.login(
+			'/save/', 'jparrish', 'Password'
+		)
+		self.assertTrue(response)
+		data = {
+			'url': 'http://www.example2.com',
+			'title': 'Test URL 2',
+			'tags': 'test-tag'
+		}
+		response = self.client.post('/save/', data)
+		self.assertEqual(response.status_code, 302)
+		response = self.client.get('/user/jparrish/')
+		self.assertTrue('http://www.example2.com/' in response.content)
+		self.assertTrue('Test URL 2' in response.content)
+		self.assertTrue('test-tag' in response.content)
